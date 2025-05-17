34de21119bdf9c9f0e5f00184faa7d7ce1a60d97
Thanujan23 <thavathanu2002@gmail.com>
2025-05-18 00:53:33 +0530

    Added backend and frontend modified part

--- backend/src/main/java/backend/repository/LearningProgressRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.repository;
-
-import backend.model.LearningProgressModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LearningProgressRepository extends MongoRepository<LearningProgressModel,String> {
-}
--- backend/src/main/java/backend/repository/PostManagementRepository.java
+++
@@ -1,8 +0,0 @@
-package backend.repository;
-
-import backend.model.PostManagementModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostManagementRepository extends MongoRepository<PostManagementModel, String> {
-   
-}
