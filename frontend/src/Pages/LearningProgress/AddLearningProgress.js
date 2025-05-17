26aa29ec83e5f3619eca35b1adeb5863facca1fc
niruthan-nps <niruthanparanthaman@gmail.com>
2025-05-17 23:53:02 +0530

    start date input from AddLearningProgress form

--- frontend/src/Pages/LearningProgress/AddLearningProgress.js
+++ frontend/src/Pages/LearningProgress/AddLearningProgress.js
@@ -73,18 +73,7 @@ function AddLearningProgress() {
             />
           </div>
 
-          <div className="pro-date-inputs">
-            <div className="pro-form-group">
-              <label>Start Date</label>
-              <input
-                className="pro-form-input"
-                name="startDate"
-                type="date"
-                value={formData.startDate}
-                onChange={handleChange}
-                required
-              />
-            </div>
+          
 
             <div className="pro-form-group">
               <label>End Date</label>
